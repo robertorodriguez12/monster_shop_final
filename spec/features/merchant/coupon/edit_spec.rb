6f240f266a56f3ff02865a5a6eae0b3923437d26
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-19 12:55:36 -0600

    create test for coupon edit

---
+++ spec/features/merchant/coupon/edit_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Coupons", type: :feature do
+  describe "As a merchant" do
+    before(:each) do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+      @user = @bike_shop.users.create(email: "c_j@email.com", password: "test", name: "Meg", city: "blah", state: "blah", address: "blah", zip: 12345, role: 1)
+      @coupon = @bike_shop.coupons.create!(name: "Bulk Discount", code:"Bulk", percent_off: 20)
+      @coupon2 = @bike_shop.coupons.create!(name: "Holiday Sale", code:"Bulk", percent_off: 10)
+    end
+
+    it "I can edit a coupon" do
+      visit '/login'
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+      click_button "Login"
+      click_on "My Coupons"
+
+      within"#coupon-#{@coupon.id}" do
+        expect(page).to have_link("Edit")
+        click_on "Edit"
+      end
+      expect(current_path).to eq("/merchant/coupons/#{@coupon.id}/edit")
+
+      fill_in 'Name', with: "Bulk"
+      click_on "Update Coupon"
+    end
+  end
+end
